773a9f6aea7a5e093bb7ff3fcef87f5073dc774b
Liliya <liliyavoloshine@gmail.com>
2021-06-25 12:28:39 +0400

    first commit

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+	// stories: [
+	// 	'../src/**/*.stories.mdx',
+	// 	'../src/**/*.stories.@(js|jsx|ts|tsx)',
+	// 	'../src/components/Task.stories.js'
+	// ],
+	stories: ['../src/components/*.stories.js'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+}
---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <TaskList/>
+  </div>
+</template>
+
+<script>
+import TaskList from './components/TaskList.vue';
+
+export default {
+  name: "App",
+  components: {
+    TaskList
+  }
+};
+</script>
+
+<style>
+@import "./index.css";
+</style>
